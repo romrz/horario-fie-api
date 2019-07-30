e28322b07427a9e55d5a407bbb2f078b22a00bb4
Romario Ramirez <romario.r@39dollarglasses.com>
2019-07-30 10:07:30 -0500

    HorarioFIE comeback!!

--- src/CurlPersistentConnection.php
+++ src/CurlPersistentConnection.php
@@ -21,16 +21,11 @@ class CurlPersistentConnection
 
         Returns the contents of the response.
     */
-    public function post($url, $fields)
+    public function get($url)
     {
-        curl_setopt($this->ch, CURLOPT_POST, 1);
-        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $fields);
         curl_setopt($this->ch, CURLOPT_URL, $url);
         curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($this->ch, CURLOPT_COOKIEJAR, 'cookies/cookies.txt');
-        curl_setopt($this->ch, CURLOPT_COOKIEFILE, 'cookies/cookies.txt');
         return curl_exec($this->ch);
     }
 
--- src/SubjectRetriever.php
+++ src/SubjectRetriever.php
@@ -6,54 +6,25 @@
 */
 class SubjectRetriever
 {
-    const URL = 'https://escolar.fie.umich.mx/20192019/estudiante/materia-sig.php';
+    const URL = 'https://escolar.fie.umich.mx/actual/estudiante/materia-rom.php';
 
     public static function all()
     {
         $fields = [
             'materia' => $subjectId,
-            // The next two lines aren't important. It's just additional information.
-            d('m9secB64TBoqWOUovogmXTgaaTC19XCFiXgbvE7Cz/s=') => d('wAJG92Q/AsOr2uHmOgy2RB9GmfE9TR7OixsMSbHNjdw='),
-            d('Q1eM11qvMDNntqPHR3KR+tGijYJND/PTw+L+DJZZcHQ=') => d('78Il3WcSsYoPhsW7gM+AoiqImj58q6sPq3hpENsMXnQ=')
-            // If you know what these are, please don't do anything bad ;)
         ];
 
         $connection = new CurlPersistentConnection;
-        $connection->post(self::URL, $fields); // Login request
-        $html = $connection->post(self::URL, $fields); // Subject info request
+        $html = $connection->get(self::URL, $fields); // Subject info request
 
-        $parser = new SubjectParser($html);
-
-        return json_encode($parser->parseAll());
+        return $html;
     }
 
     public static function get($subjectId)
     {
-        $fields = [
-            'materia' => $subjectId,
-            // The next two lines aren't important. It's just additional information.
-            d('m9secB64TBoqWOUovogmXTgaaTC19XCFiXgbvE7Cz/s=') => d('wAJG92Q/AsOr2uHmOgy2RB9GmfE9TR7OixsMSbHNjdw='),
-            d('Q1eM11qvMDNntqPHR3KR+tGijYJND/PTw+L+DJZZcHQ=') => d('78Il3WcSsYoPhsW7gM+AoiqImj58q6sPq3hpENsMXnQ=')
-            // If you know what these are, please don't do anything bad ;)
-        ];
-
         $connection = new CurlPersistentConnection;
-        $connection->post(self::URL, $fields); // Login request
-        $html = $connection->post(self::URL, $fields); // Subject info request
+        $html = $connection->get(self::URL . '?materia=' . $subjectId);
 
-        $parser = new SubjectParser($html);
-        $subject = $parser->parse(); 
-
-        return json_encode($subject);
+        return $html;
     }
-}
-
-function e($text)
-{  
-    return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, 'asdfghjklasdfghj', $text, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
-}
-
-function d($text)
-{  
-    return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, 'asdfghjklasdfghj', base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
 }
